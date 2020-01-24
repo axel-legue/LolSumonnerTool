ae12dba34b04172e8dcfd444e3c2834676760448
LEGUE Axel <axel.legue31@gmail.com>
2020-01-24 11:00:26 +0100

    feat: refactor viewmodels to kotlin

---
+++ app/src/main/java/com/legue/axel/lolsummonertool/database/viewmodel/ItemGoldViewModel.kt
@@ -0,0 +1,18 @@
+package com.legue.axel.lolsummonertool.database.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+
+import com.legue.axel.lolsummonertool.database.SummonerToolDatabase
+import com.legue.axel.lolsummonertool.database.model.item.ItemGold
+
+class ItemGoldViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val database: SummonerToolDatabase = SummonerToolDatabase.getInstance(this.getApplication())
+
+    fun getItemGoldByItemId(itemId: Int): LiveData<ItemGold> {
+        return database.itemGoldDao().getItemGoldByItemId(itemId)
+    }
+}
+
